8847d8fa229ea83f9afa43f6f571535dfae49b43
Murfin <joonas.kriisk@hotmail.com>
2014-11-05 13:18:15 +0200

    works

--- mustached-nemesis/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
+++ mustached-nemesis/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
@@ -4,15 +4,20 @@ import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 import ee.ut.math.tvt.salessystem.ui.panels.PurchaseItemPanel;
+import ee.ut.math.tvt.salessystem.ui.panels.ConfirmOrderPanel;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -168,6 +173,9 @@ public class PurchaseTab {
 		try {
 			log.debug("Contents of the current basket:\n"
 					+ model.getCurrentPurchaseTableModel());
+			JOptionPane.showOptionDialog(null, new ConfirmOrderPanel(model),
+					"Confirm order dialog", JOptionPane.DEFAULT_OPTION,
+					JOptionPane.DEFAULT_OPTION, null, new Object[] {}, null);
 			domainController.submitCurrentPurchase(model
 					.getCurrentPurchaseTableModel().getTableRows());
 			endSale();
