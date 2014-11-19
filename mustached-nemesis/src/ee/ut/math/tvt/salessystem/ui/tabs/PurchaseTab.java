8d606f6fda3e289816dabf29d705a1010d121829
Murfin <joonas.kriisk@hotmail.com>
2014-11-19 13:33:43 +0200

    retarded stuff

--- mustached-nemesis/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
+++ mustached-nemesis/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
@@ -3,16 +3,21 @@ package ee.ut.math.tvt.salessystem.ui.tabs;
 import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import ee.ut.math.tvt.salessystem.ui.panels.ConfirmOrderPanel;
 import ee.ut.math.tvt.salessystem.ui.panels.PurchaseItemPanel;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -21,31 +26,29 @@ import org.apache.log4j.Logger;
  */
 public class PurchaseTab {
 
-  private static final Logger log = Logger.getLogger(PurchaseTab.class);
+	private static final Logger log = Logger.getLogger(PurchaseTab.class);
 
-  private final SalesDomainController domainController;
+	private final SalesDomainController domainController;
 
-  private JButton newPurchase;
+	private JButton newPurchase;
 
-  private JButton submitPurchase;
+	private JButton submitPurchase;
 
-  private JButton cancelPurchase;
+	private JButton cancelPurchase;
 
-  private PurchaseItemPanel purchasePane;
+	private PurchaseItemPanel purchasePane;
 
-  private SalesSystemModel model;
-
+	private SalesSystemModel model;
 
 	public PurchaseTab(SalesDomainController controller, SalesSystemModel model) {
 		this.domainController = controller;
 		this.model = model;
 	}
 
-
-  /**
-   * The purchase tab. Consists of the purchase menu, current purchase dialog and
-   * shopping cart table.
-   */
+	/**
+	 * The purchase tab. Consists of the purchase menu, current purchase dialog
+	 * and shopping cart table.
+	 */
 	public Component draw() {
 		JPanel panel = new JPanel();
 
@@ -63,10 +66,7 @@ public class PurchaseTab {
 		return panel;
 	}
 
-
-
-
-  // The purchase menu. Contains buttons "New purchase", "Submit", "Cancel".
+	// The purchase menu. Contains buttons "New purchase", "Submit", "Cancel".
 	private Component getPurchaseMenuPane() {
 		JPanel panel = new JPanel();
 
@@ -88,8 +88,7 @@ public class PurchaseTab {
 		return panel;
 	}
 
-
-  // Creates the button "New purchase"
+	// Creates the button "New purchase"
 	private JButton createNewPurchaseButton() {
 		JButton b = new JButton("New purchase");
 		b.addActionListener(new ActionListener() {
@@ -101,7 +100,7 @@ public class PurchaseTab {
 		return b;
 	}
 
-  // Creates the "Confirm" button
+	// Creates the "Confirm" button
 	private JButton createConfirmButton() {
 		JButton b = new JButton("Confirm");
 		b.addActionListener(new ActionListener() {
@@ -114,8 +113,7 @@ public class PurchaseTab {
 		return b;
 	}
 
-
-  // Creates the "Cancel" button
+	// Creates the "Cancel" button
 	private JButton createCancelButton() {
 		JButton b = new JButton("Cancel");
 		b.addActionListener(new ActionListener() {
@@ -128,16 +126,12 @@ public class PurchaseTab {
 		return b;
 	}
 
+	/*
+	 * === Event handlers for the menu buttons (get executed when the buttons
+	 * are clicked)
+	 */
 
-
-
-
-  /* === Event handlers for the menu buttons
-   *     (get executed when the buttons are clicked)
-   */
-
-
-  /** Event handler for the <code>new purchase</code> event. */
+	/** Event handler for the <code>new purchase</code> event. */
 	protected void newPurchaseButtonClicked() {
 		log.info("New sale process started");
 		try {
@@ -148,8 +142,7 @@ public class PurchaseTab {
 		}
 	}
 
-
-  /**  Event handler for the <code>cancel purchase</code> event. */
+	/** Event handler for the <code>cancel purchase</code> event. */
 	protected void cancelPurchaseButtonClicked() {
 		log.info("Sale cancelled");
 		try {
@@ -161,13 +154,14 @@ public class PurchaseTab {
 		}
 	}
 
-
-  /** Event handler for the <code>submit purchase</code> event. */
+	/** Event handler for the <code>submit purchase</code> event. */
 	protected void submitPurchaseButtonClicked() {
-		log.info("Sale complete");
 		try {
 			log.debug("Contents of the current basket:\n"
 					+ model.getCurrentPurchaseTableModel());
+			JOptionPane.showOptionDialog(null, new ConfirmOrderPanel(model),
+					"Confirm order dialog", JOptionPane.DEFAULT_OPTION,
+					JOptionPane.DEFAULT_OPTION, null, new Object[] {}, null);
 			domainController.submitCurrentPurchase(model
 					.getCurrentPurchaseTableModel().getTableRows());
 			endSale();
@@ -177,13 +171,12 @@ public class PurchaseTab {
 		}
 	}
 
+	/*
+	 * === Helper methods that bring the whole purchase-tab to a certain state
+	 * when called.
+	 */
 
-
-  /* === Helper methods that bring the whole purchase-tab to a certain state
-   *     when called.
-   */
-
-  // switch UI to the state that allows to proceed with the purchase
+	// switch UI to the state that allows to proceed with the purchase
 	private void startNewSale() {
 		purchasePane.reset();
 
@@ -193,7 +186,7 @@ public class PurchaseTab {
 		newPurchase.setEnabled(false);
 	}
 
-  // switch UI to the state that allows to initiate new purchase
+	// switch UI to the state that allows to initiate new purchase
 	private void endSale() {
 		purchasePane.reset();
 
@@ -203,50 +196,47 @@ public class PurchaseTab {
 		purchasePane.setEnabled(false);
 	}
 
+	/*
+	 * === Next methods just create the layout constraints objects that control
+	 * the the layout of different elements in the purchase tab. These
+	 * definitions are brought out here to separate contents from layout, and
+	 * keep the methods that actually create the components shorter and cleaner.
+	 */
 
+	private GridBagConstraints getConstraintsForPurchaseMenu() {
+		GridBagConstraints gc = new GridBagConstraints();
 
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		gc.anchor = GridBagConstraints.NORTH;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.weightx = 1.0d;
+		gc.weighty = 0d;
 
-  /* === Next methods just create the layout constraints objects that control the
-   *     the layout of different elements in the purchase tab. These definitions are
-   *     brought out here to separate contents from layout, and keep the methods
-   *     that actually create the components shorter and cleaner.
-   */
+		return gc;
+	}
 
-  private GridBagConstraints getConstraintsForPurchaseMenu() {
-    GridBagConstraints gc = new GridBagConstraints();
+	private GridBagConstraints getConstraintsForPurchasePanel() {
+		GridBagConstraints gc = new GridBagConstraints();
 
-    gc.fill = GridBagConstraints.HORIZONTAL;
-    gc.anchor = GridBagConstraints.NORTH;
-    gc.gridwidth = GridBagConstraints.REMAINDER;
-    gc.weightx = 1.0d;
-    gc.weighty = 0d;
+		gc.fill = GridBagConstraints.BOTH;
+		gc.anchor = GridBagConstraints.NORTH;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.weightx = 1.0d;
+		gc.weighty = 1.0;
 
-    return gc;
-  }
+		return gc;
+	}
 
+	// The constraints that control the layout of the buttons in the purchase
+	// menu
+	private GridBagConstraints getConstraintsForMenuButtons() {
+		GridBagConstraints gc = new GridBagConstraints();
 
-  private GridBagConstraints getConstraintsForPurchasePanel() {
-    GridBagConstraints gc = new GridBagConstraints();
+		gc.weightx = 0;
+		gc.anchor = GridBagConstraints.CENTER;
+		gc.gridwidth = GridBagConstraints.RELATIVE;
 
-    gc.fill = GridBagConstraints.BOTH;
-    gc.anchor = GridBagConstraints.NORTH;
-    gc.gridwidth = GridBagConstraints.REMAINDER;
-    gc.weightx = 1.0d;
-    gc.weighty = 1.0;
-
-    return gc;
-  }
-
-
-  // The constraints that control the layout of the buttons in the purchase menu
-  private GridBagConstraints getConstraintsForMenuButtons() {
-    GridBagConstraints gc = new GridBagConstraints();
-
-    gc.weightx = 0;
-    gc.anchor = GridBagConstraints.CENTER;
-    gc.gridwidth = GridBagConstraints.RELATIVE;
-
-    return gc;
-  }
+		return gc;
+	}
 
 }
